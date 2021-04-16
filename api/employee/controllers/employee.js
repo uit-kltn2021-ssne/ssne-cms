688798068391a252362052f58664f3234cdf9dc8
Trung Hieu <trunghieu2570@gmail.com>
2021-04-16 23:36:04 +0700

    add synonyms for names

--- api/employee/controllers/employee.js
+++ api/employee/controllers/employee.js
@@ -5,25 +5,17 @@ const dialogflow = require("@google-cloud/dialogflow");
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
- var combine = function(a, min) {
-    var fn = function(n, src, got, all) {
-        if (n == 0) {
-            if (got.length > 0) {
-                all[all.length] = got;
-            }
-            return;
-        }
-        for (var j = 0; j < src.length; j++) {
-            fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
-        }
-        return;
+var namePattern = (arr) => {
+    let result = [];
+    if (arr.length > 0)
+        result.push(arr[arr.length - 1]);
+    if (arr.length > 1)
+        result.push(`${arr[arr.length - 2]} ${arr[arr.length - 1]}`);
+    if (arr.length > 2) {
+        result.push(`${arr[0]} ${arr[arr.length - 1]}`);
+        result.push(arr.join(' '));
     }
-    var all = [];
-    for (var i = min; i < a.length; i++) {
-        fn(i, a, [], all);
-    }
-    all.push(a);
-    return all;
+    return result;
 }
 
 module.exports = {
@@ -40,7 +32,7 @@ module.exports = {
         employees.forEach(element => {
             const entity = {
                 value: element.name ,
-                synonyms: combine(element.name.split(" "), 1)
+                synonyms: namePattern(element.name.split(" "))
             }
             entityList.push(entity);
         });
